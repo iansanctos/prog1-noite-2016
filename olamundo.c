2538716fc67dc8fa66bd8ea04d6a0dd80af6c442
Ian Sanctos <barbosa.ian@gmail.com>
2016-04-19 00:56:04 +0000

    meu primeiro commit no github

---
+++ olamundo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("\n Boa Noite! ");   
+  printf("\n Meu nome e Ian "); 
+  printf("\n Este e meu segundo programa em C ! \n"); 
+    
+    return 0;
+}
